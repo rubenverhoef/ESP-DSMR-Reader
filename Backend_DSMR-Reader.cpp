f8854ddb995e8ea021d4d56b72ba9516a937119a
Ruben Verhoef <ruben_verhoef@hotmail.com>
2020-01-20 17:59:41 +0100

    Support more than 2 digits for double type

--- Backend_DSMR-Reader.cpp
+++ Backend_DSMR-Reader.cpp
@@ -4,6 +4,9 @@
 #include "settings.h"
 #include "Backend_DSMR-Reader.h"
 
+// Function prototype
+String Double2String(double metric);
+
 // **********************************
 // * Backend DSMR-Reader            *
 // **********************************
@@ -20,75 +23,82 @@ String Format_HTTP_data(MyData data, MyData LastData)
     if (data.power_delivered_present) // Always Required
     {
         httpData += "&electricity_currently_delivered=";
-        httpData += data.power_delivered;
+        httpData += Double2String(data.power_delivered);
     }
     if (data.power_returned_present) // Always Required
     {
         httpData += "&electricity_currently_returned=";
-        httpData += data.power_returned;
+        httpData += Double2String(data.power_returned);
     }
     if (data.energy_delivered_tariff1_present) // Always Required
     {
         httpData += "&electricity_delivered_1=";
-        httpData += data.energy_delivered_tariff1;
+        httpData += Double2String(data.energy_delivered_tariff1);
     }
     if (data.energy_delivered_tariff2_present) // Always Required
     {
         httpData += "&electricity_delivered_2=";
-        httpData += data.energy_delivered_tariff2;
+        httpData += Double2String(data.energy_delivered_tariff2);
     }
     if (data.energy_returned_tariff1_present) // Always Required
     {
         httpData += "&electricity_returned_1=";
-        httpData += data.energy_returned_tariff1;
+        httpData += Double2String(data.energy_returned_tariff1);
     }
     if (data.energy_returned_tariff2_present) // Always Required
     {
         httpData += "&electricity_returned_2=";
-        httpData += data.energy_returned_tariff2;
+        httpData += Double2String(data.energy_returned_tariff2);
     }
     if (data.power_delivered_l1_present && data.power_delivered_l1 != LastData.power_delivered_l1)
     {
         httpData += "&phase_currently_delivered_l1=";
-        httpData += data.power_delivered_l1;
+        httpData += Double2String(data.power_delivered_l1);
     }
     if (data.power_delivered_l2_present && data.power_delivered_l2 != LastData.power_delivered_l2)
     {
         httpData += "&phase_currently_delivered_l2=";
-        httpData += data.power_delivered_l2;
+        httpData += Double2String(data.power_delivered_l2);
     }
     if (data.power_delivered_l3_present && data.power_delivered_l3 != LastData.power_delivered_l3)
     {
         httpData += "&phase_currently_delivered_l3=";
-        httpData += data.power_delivered_l3;
+        httpData += Double2String(data.power_delivered_l3);
     }
     if (data.power_returned_l1_present && data.power_returned_l1 != LastData.power_returned_l1)
     {
         httpData += "&phase_currently_returned_l1=";
-        httpData += data.power_returned_l1;
+        httpData += Double2String(data.power_returned_l1);
     }
     if (data.power_returned_l2_present && data.power_returned_l2 != LastData.power_returned_l2)
     {
         httpData += "&phase_currently_returned_l2=";
-        httpData += data.power_returned_l2;
+        httpData += Double2String(data.power_returned_l2);
     }
     if (data.power_returned_l3_present && data.power_returned_l3 != LastData.power_returned_l3)
     {
         httpData += "&phase_currently_returned_l3=";
-        httpData += data.power_returned_l3;
+        httpData += Double2String(data.power_returned_l3);
     }
     if (data.gas_delivered_present && data.gas_delivered != LastData.gas_delivered)
     {
         httpData += "&extra_device_timestamp=";
         httpData += formatDate(data.gas_delivered.timestamp);
         httpData += "&extra_device_delivered=";
-        httpData += data.gas_delivered;
+        httpData += Double2String(data.gas_delivered);
     }
 
     /* Remove first char of string (the &) */
     httpData = httpData.substring(1, httpData.length());
     Debug.println(httpData);
     return httpData;
+}
+
+String Double2String(double metric)
+{
+    char output[64+1];
+    snprintf(output, sizeof(output), "%g", metric);
+    return String(output);
 }
 
 extern char dsmrAPI[70]; // API key for DSMR-Reader backend
